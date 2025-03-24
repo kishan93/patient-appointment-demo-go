cccd513db36510245117c157cd61b193205cae31
Kishan Nath <kishansnath93@gmail.com>
2025-03-25 00:15:31 +0530

    fixed formating

--- internal/routes/patient-response.go
+++ internal/routes/patient-response.go
@@ -3,7 +3,7 @@ package routes
 import "patient-appointment-demo-go/internal/database"
 
 type PatientResponse struct {
-	ID    int64  `json:"id"`
+	ID      int64   `json:"id"`
 	Name    string  `json:"name"`
 	Phone   string  `json:"phone"`
 	Email   string  `json:"email"`
@@ -19,26 +19,26 @@ func PatientDbToResponse(data database.Patient) PatientResponse {
 	height, _ := data.Height.Float64Value()
 
 	return PatientResponse{
-		ID:   int64(data.ID),
-		Name:   data.Name,
-		Phone:  data.Phone.String,
-		Email:  data.Email,
-		Age:    data.Age.Int16,
-		Weight: weight.Float64,
-		Height: height.Float64,
-        Gender: data.Gender.String,
-        Address: data.Address.String,
+		ID:      int64(data.ID),
+		Name:    data.Name,
+		Phone:   data.Phone.String,
+		Email:   data.Email,
+		Age:     data.Age.Int16,
+		Weight:  weight.Float64,
+		Height:  height.Float64,
+		Gender:  data.Gender.String,
+		Address: data.Address.String,
 	}
 }
 
 func PatientDbArrayToResponse(data []database.Patient) []PatientResponse {
 
-    patients := make([]PatientResponse, len(data))
+	patients := make([]PatientResponse, len(data))
 
-    for i,item := range data {
-        patients[i] = PatientDbToResponse(item)
-    }
+	for i, item := range data {
+		patients[i] = PatientDbToResponse(item)
+	}
 
-    return patients
+	return patients
 
 }
